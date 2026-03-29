2a83fad953b689593a59748b730c422672eb01de
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 22:00:53 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let HF_TOKEN;
 
 function getToken() {
   if (!HF_TOKEN) {
-    HF_TOKEN = prompt("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:");
+    HF_TOKEN = prompt("Masukin HuggingFace token lo:");
   }
   return HF_TOKEN;
 }
